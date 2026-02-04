09a9913f8bcfc4afad14e69c5d99e5001ac6e483
Vladislav Tuchkov <tuckovv553@gmail.com>
2026-02-04 14:49:18 +0700

    new

--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.processing import filter_by_state, sort_by_date
 
 
 def main():
@@ -12,6 +13,34 @@ def main():
     masked_card = get_mask_card_number(card_number)
     print(f"Маскированный номер карты: {masked_card}")
 
+    # Вывод даты
+    operations = [
+        {
+            "id": 1,
+            "state": "EXECUTED",
+            "date": "2024-03-11T02:26:18.671407",
+        },
+        {
+            "id": 2,
+            "state": "CANCELED",
+            "date": "2023-01-10T10:00:00.000000",
+        },
+        {
+            "id": 3,
+            "state": "EXECUTED",
+            "date": "2024-05-01T12:00:00.000000",
+        },
+    ]
+
+    # 1. Фильтрация
+    filtered = filter_by_state(operations)
+    print("После фильтрации:")
+    print(filtered)
+
+    # 2. Сортировка
+    sorted_ops = sort_by_date(filtered)
+    print("\nПосле сортировки:")
+    print(sorted_ops)
 
 if __name__ == "__main__":
     main()
